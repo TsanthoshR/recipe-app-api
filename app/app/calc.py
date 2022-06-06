c8f67c6b514ba024f2a9b542ea4fdc13a876ece1
TSanthosh <santhosht5290@gmail.com>
2022-06-07 00:26:45 +0530

    removed linting errors

--- app/app/calc.py
+++ app/app/calc.py
@@ -3,16 +3,18 @@ calculator function
 
 """
 
+
 def add(x, y):
     """Add x and y
 
     Args:
-        x (_type_): first number 
+        x (_type_): first number
         y (_type_): second number
     """
     return x + y
 
-def subtract(x,y):
+
+def subtract(x, y):
     """subtract y from x"""
-    
+
     return x-y
--- app/app/tests.py
+++ app/app/tests.py
@@ -1,5 +1,5 @@
 """
-Write unit Tests here 
+Write unit Tests here
 
 """
 
@@ -7,18 +7,16 @@ from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTest(SimpleTestCase):
     """Test the calc module"""
-    
+
     def test_add_number(self):
         """Test adding numbers together """
-        res = calc.add(5,6)
-        
-        self.assertEqual(res,11)
-        
+        res = calc.add(5, 6)
+        self.assertEqual(res, 11)
+
     def test_subtract_number(self):
         """ Test subtracting numbers"""
-        res = calc.subtract(10,15)
-        
-        self.assertEqual(res,-5)        
-        
+        res = calc.subtract(10, 15)
+        self.assertEqual(res, -5)
