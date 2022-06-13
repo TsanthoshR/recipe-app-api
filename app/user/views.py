cc6336791aab771eb0d8084a9716f4d7f5cf72f7
TSanthosh <santhosht5290@gmail.com>
2022-06-14 01:27:07 +0530

    user api views, serializer and urls created

--- app/user/views.py
+++ app/user/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+"""Views for the user api"""
+
+from rest_framework import generics
+
+from user.serializers import UserSerializer
+# from django.shortcuts import render
 
 # Create your views here.
+
+
+class CreateUserView(generics.CreateAPIView):
+    # HTTP POST to create a new obj in DB
+    """Create a new user in the system."""
+    serializer_class = UserSerializer
